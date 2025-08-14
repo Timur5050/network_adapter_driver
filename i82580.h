55f842fab1aa55a8c45d67cc2bd13e6f8c5ac4df
timur5050 <stukantimur811@gmail.com>
2025-08-14 17:09:18 +0300

    headed file with including general headers and main struct of driver

---
+++ i82580.h
@@ -0,0 +1,55 @@
+#ifndef _I82580_H_
+#define _I82580_H_
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/pci.h>
+#include <linux/msi.h>
+#include <linux/interrupt.h>
+#include <linux/io.h>
+#include <linux/slab.h>
+#include <linux/netdevice.h>
+#include <linux/etherdevice.h>
+
+struct i82580_adapter;
+
+#define I82580_BAR_MMIO 0
+
+struct i82580_ring {
+    void *desc;                     // list if descriptors ( DMA )
+    dma_addr_t dma;                 // DMA-address for device
+    unsigned int count;             // amount of descriptors in ring
+    unsigned int next_to_use;       // index of next entry to use
+    unsigned int next_to_clean;     // index of next entry to clean
+
+    struct sk_buff **skbs;          // connected sk_buff for each descriptor
+};
+
+
+struct i82580_adapter {
+
+    /* OS defigned structs */
+    struct net_device *netdev;
+    struct pci_dev *pdev;
+
+    /* tx */
+    struct i82580_ring *tx_ring;
+    int num_tx_queues;
+    spinlock_t tx_lock;
+
+    /* rx */
+    struct napi_struct napi;
+    struct i82580_ring *rx_ring;
+    int num_rx_queues;
+    spinlock_t rx_lock;
+
+    /* hw mem */
+    void __iomem *hw_addr;
+
+    /* interrupts */
+    struct msix_entry *msix_entries;
+    int num_vectors;
+};
+
+#endif /* _I82580_H_ */
