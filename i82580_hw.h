7df0e32666ef2c836f3e289f71da64b540fe5838
timur5050 <stukantimur811@gmail.com>
2025-08-21 13:31:34 +0300

    changed struct to advanced from legacy

--- i82580_hw.h
+++ i82580_hw.h
@@ -4,44 +4,39 @@
 #include <linux/types.h>
 
 struct i82580_adv_tx_desc {
-    __le64 buffer_addr;   // Address of data buffer
+    __le64 buffer_addr;    /* Data buffer address */
     union {
-        __le32 data;      // For read format
+        __le32 data;
         struct {
-            __le16 length;  // Data buffer length
-            u8     cso;     // Checksum offset
-            u8     cmd;     // Command flags (EOP, IFCS, RS, etc.)
+            __le16 length;    /* Data buffer length */
+            u8 cso;           /* Checksum offset */
+            u8 cmd;           /* Descriptor control (EOP, IFCS, RS, etc.) */
         } flags;
-    } lower;
-    union {
-        __le32 data;      // For write-back format
-        struct {
-            u8 status;     // Descriptor status
-            u8 css;        // Checksum start
-            __le16 special;
-        } wb;
-    } upper;
-};
-
-struct i82580_adv_rx_desc {
-    __le64 pkt_addr;    // Packet buffer address
-    __le64 hdr_addr;    // Header buffer address (can be 0 if unused)
-    union {
-        __le32 data;    // For write-back format
-        struct {
-            __le16 length;   // Packet length
-            __le16 csum;     // Packet checksum
-        } lengths;
     } lower;
     union {
         __le32 data;
         struct {
-            u8 status;       // Descriptor status (DD, EOP, etc.)
-            u8 errors;       // Error flags
-            __le16 vlan;     // VLAN tag if enabled
-        } wb;
+            u8 status;        /* Descriptor status */
+            u8 css;           /* Checksum start */
+            __le16 vlan;      /* VLAN tag */
+        } fields;
     } upper;
 };
-
+union i82580_adv_rx_desc {
+    struct {                   /* READ (driver -> HW) */
+        __le64 pkt_addr;
+        __le64 hdr_addr;
+    } read;
+    struct {                   /* WRITE-BACK (HW -> driver) */
+        __le32 mrq;
+        union {
+            __le32 rss;
+            __le32 ip_id;
+        } hi_dword;
+        __le32 status_error;
+        __le16 length;
+        __le16 vlan;
+    } wb;
+};
 
 #endif /* _I82580_HW_H_ */
